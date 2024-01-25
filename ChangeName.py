c83b19d045fb688f01ed209316f03a7126fa3c14
MHosein94 <mh.taghavi94@gmail.com>
2024-01-24 19:43:06 -0500

    Add Emojies

--- ChangeName.py
+++ ChangeName.py
@@ -4,7 +4,7 @@ configs_files = os.listdir()
 for config_file in configs_files:
     if '.x' in config_file:
         new_proxies=''
-        service_name='Sakura' if config_file != 'Sequoia.x' else 'Sequoia'
+        service_name='🌸Sakura' if config_file != 'Sequoia.x' else '🌲Sequoia'
         with open(config_file, 'r') as f:
             proxies = f.readlines()
             for i, proxy in enumerate(proxies):
